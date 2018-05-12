c6829990ce990cb164e750a2b7f40c981382417e
rmchen-12 <rmchen1993@163.com>
2018-05-13 01:48:53 +0800

    test1

--- a.js
+++ a.js
@@ -1,3 +1,46 @@
-//同步测试
+function foo() {
+    console.log(this.a);
+}
 
-console.log(1);
+var obj = {
+    a: 2
+}
+
+// foo.call(obj)
+
+function foo() {
+    console.log(this.a);
+}
+
+var obj = {
+    a: 2
+}
+
+var bar = function () {
+    foo.call(obj)
+}
+
+// bar()
+
+// setTimeout(bar, 100);
+
+// bar.call(global)
+
+function foo() {
+    console.log(this.a);
+}
+
+var obj = {
+    a:2,
+    foo:foo
+}
+
+var a = 'hello world'
+
+var a = function () {
+    return foo.apply(obj)
+}
+
+setTimeout(()=>{
+    a()
+}, 100);
